247a6c4281c12ceadbf5be279cda21689c8ecbd5
Yuhee2020 <62483987+Yuhee2020@users.noreply.github.com>
2022-03-27 22:46:21 +0300

    New design

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -3,7 +3,7 @@ import  s from "./Header.module.css";
 
 function Header() {
     return <header className={s.header}>
-        <img src="https://upload.wikimedia.org/wikipedia/commons/1/14/Free_%28Logo%29.png"/>
+        <img src="logo_size.jpg"/>
 
     </header>
 }
