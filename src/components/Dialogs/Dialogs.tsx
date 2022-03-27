c01c7274938e0ac935d458d8a3ce55c275045801
Yuhee2020 <62483987+Yuhee2020@users.noreply.github.com>
2022-03-27 21:45:06 +0300

    Dialogs

--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -1,8 +1,32 @@
 import s from "./Dialogs.module.css"
 
-function Dialogs(){
-    return <div>
-        dialogs
+function Dialogs() {
+    return <div className={s.dialogs}>
+        <div className={s.users}>
+            <div className={s.user + " " + s.active}>
+                Dimas
+            </div>
+            <div className={s.user}>
+                Dasha
+            </div>
+            <div className={s.user}>
+                Vania
+            </div>
+            <div className={s.user}>
+                Valera
+            </div>
+            <div className={s.user}>
+                Irina
+            </div>
+        </div>
+        <div className={s.messages}>
+            <div className={s.message}>Hello!</div>
+            <div className={s.message}>How are you?</div>
+            <div className={s.message}>Where are you?</div>
+            <div className={s.message}>Yanky go home!</div>
+        </div>
+
     </div>
 }
+
 export default Dialogs
