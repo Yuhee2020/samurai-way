8476f6332b0e0e4ea05d9f8848db9efff54e7b63
Yuhee2020 <62483987+Yuhee2020@users.noreply.github.com>
2022-04-02 21:29:55 +0300

    New design2

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,10 +1,11 @@
 import React from "react";
-import  s from "./Header.module.css";
+import s from "./Header.module.css";
+import logo from "./logo_size.jpg"
 
 function Header() {
     return <header className={s.header}>
-        <img src="logo_size.jpg"/>
-
+        <img src={logo}/>
+        <span className={s.title}>Social Network </span>
     </header>
 }
 
