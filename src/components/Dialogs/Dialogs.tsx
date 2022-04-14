5ec41adfffecb1a024c2f433f5cbeeba2960cef5
Yuhee2020 <62483987+Yuhee2020@users.noreply.github.com>
2022-04-14 20:21:00 +0300

    data in object

--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -4,19 +4,32 @@ import {Dialog} from "./Dialog/Dialog";
 import { Message } from "./Message/Message";
 
 function Dialogs() {
+
+    const dialogsData =[
+        {name:"Dimas",id:1},
+        {name:"Dasha",id:2},
+        {name:"Vania",id:3},
+        {name:"Valera",id:4},
+        {name:"Irina",id:5},
+    ]
+
+    const messagesData = [
+        {id:1, message:"Hello"},
+        {id:2, message:"How are you?"},
+        {id:3, message:"Where are you?"},
+        {id:4, message:"Good bye!"}
+    ]
+
     return <div className={s.dialogs}>
         <div className={s.dialogsItems}>
-            <Dialog name={"Dimas"} id={1}/>
-            <Dialog name={"Dasha"} id={2}/>
-            <Dialog name={"Vania"} id={3}/>
-            <Dialog name={"Valera"} id={4}/>
-            <Dialog name={"Irina"} id={5}/>
+            <Dialog name={dialogsData[0].name} id={dialogsData[0].id}/>
+            <Dialog name={dialogsData[1].name} id={dialogsData[1].id}/>
+
         </div>
         <div className={s.messages}>
-            <Message message={"Hello"}/>
-            <Message message={"How are you?"}/>
-            <Message message={"Where are you?"}/>
-            <Message message={"Good bye!"}/>
+            <Message message={messagesData[0].message}/>
+            <Message message={messagesData[1].message}/>
+
 
         </div>
 
--- src/components/Profile/MyPosts/MyPosts.tsx
+++ src/components/Profile/MyPosts/MyPosts.tsx
@@ -3,6 +3,14 @@ import s from "./MyPosts.module.css"
 import Post from "./Post/Post";
 
 function MyPosts() {
+
+    const postData = [
+        {message: "Hi, how are you?", likesCount: 20},
+        {message: "It is my first post", likesCount: 15},
+        {message: "Hello, who are you?", likesCount: 30}
+
+    ]
+
     return <div>
         My posts
         <div>
@@ -11,9 +19,9 @@ function MyPosts() {
             <button>Remove</button>
         </div>
         <div className={s.posts}>
-            <Post message={"Hi, how are you?"} likesCount={20}/>
-            <Post message={"It is my first post"} likesCount={15}/>
-            <Post message={"Hello, who are you?"} likesCount={30}/>
+            <Post message={postData[0].message} likesCount={postData[0].likesCount}/>
+            <Post message={postData[1].message} likesCount={postData[1].likesCount}/>
+
 
         </div>
     </div>
